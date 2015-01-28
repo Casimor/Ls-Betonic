2fa8d37421081feb5b6012d18fe466f0b4f58ec3
Oscar CHASE <ochase@student.42.fr>
2015-01-28 20:58:26 +0100

    super changes

--- srcs/parser.c
+++ srcs/parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bchevali <bchevali@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ochase <ochase@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/01/28 11:28:52 by ochase            #+#    #+#             */
-/*   Updated: 2015/01/28 19:13:37 by bchevali         ###   ########.fr       */
+/*   Updated: 2015/01/28 20:55:52 by ochase           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,56 @@
 #include <stdio.h>
 static void	parse_cmd(char *cmd, t_opt *opt)
 {
+	cmd++;
 	while (*cmd)
 	{
-		if (!opt->l_flag)
-			opt->l_flag |= (*cmd == 'l') ? 1 : 0;
-		if (!opt->a_flag)
-			opt->a_flag |= (*cmd == 'a') ? 1 : 0;
-		if (!opt->r_flag)
-			opt->r_flag |= (*cmd == 'r') ? 1 : 0;
-		if (!opt->t_flag)
-			opt->t_flag |= (*cmd == 't') ? 1 : 0;
-		if (!opt->re_flag)
-			opt->re_flag |= (*cmd == 'R') ? 1 : 0;
+		if (!ft_strchr("lartR", *cmd))
+			exit(0);
+		opt->l_flag |= (*cmd == 'l');
+		opt->a_flag |= (*cmd == 'a');
+		opt->r_flag |= (*cmd == 'r');
+		opt->t_flag |= (*cmd == 't');
+		opt->re_flag |= (*cmd == 'R');
 		cmd++;
+	}
+}
+
+t_list		*parser(char **argv, t_opt *opt)
+{
+	t_list	*file_lst;
+	t_list	*err_lst;
+	size_t	index;
+
+	file_lst = 0;
+	err_lst = 0;
+	index = 1;
+	ft_bzero(opt, sizeof(t_opt));
+	while (argv[index] && argv[index][0] == '-')
+		parse_cmd(argv[index++], opt);
+	while (argv[index])
+	{
+		if (opendir(argv[index]))
+			ft_lstpushback(&file_lst,
+				ft_lstnew(argv[index], ft_strlen(argv[index])));
+		else
+			ft_lstpushback(&err_lst,
+				ft_lstnew(argv[index], ft_strlen(argv[index])));
+		index++;
 	}
 	printf("l: %d\n", opt->l_flag);
 	printf("a: %d\n", opt->a_flag);
 	printf("r: %d\n", opt->r_flag);
 	printf("t: %d\n", opt->t_flag);
 	printf("R: %d\n", opt->re_flag);
-}
-
-t_list		*parser(char **argv, t_opt *opt)
-{
-	t_list	*index_lst;
-	size_t	index;
-
-	index_lst = 0;
-	index = 1;
-	opt->l_flag = 0;
-	opt->a_flag = 0;
-	opt->r_flag = 0;
-	opt->t_flag = 0;
-	opt->re_flag = 0;
-	while (argv[index])
+	while (file_lst)
 	{
-		if (argv[index][0] == '-')
-			parse_cmd(argv[index], opt);
-		else
-			ft_lstpushback(&index_lst, ft_lstnew(argv[index], ft_strlen(argv[index])));
-		index++;
+		printf("good: %s\n", (char*)file_lst->content);
+		file_lst = file_lst->next;
 	}
-	return (index_lst);
+	while (err_lst)
+	{
+		printf("error: %s\n", (char*)err_lst->content);
+		err_lst = err_lst->next;
+	}
+	return (file_lst);
 }

7b130f07c738bfa56afae92c1af78400ef70d5ad
Benoit CHEVALIER <bchevali@e2r11p12.42.fr>
2015-01-28 18:43:19 +0100

    Betonic commit

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ochase <ochase@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bchevali <bchevali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/30 15:01:57 by ochase            #+#    #+#             */
-/*   Updated: 2015/01/28 11:31:10 by ochase           ###   ########.fr       */
+/*   Updated: 2015/01/28 18:27:01 by bchevali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,41 +14,17 @@
 
 int		main(int argc, char **argv)
 {
-	t_dirent		*dirent;
-	DIR				*dir;
-	t_list			*list;
-	t_info			*info;
+	t_opt	opt;
+	t_list	*n_dir;
+	size_t	nb_dir;
 
 	(void)argc;
-	parser(argv);
-	list = NULL;
-	dir = opendir (".");
-	while ((dirent = readdir(dir)))
-	{
-		if (dirent->d_name[0] != '.')
-		{
-			info = ft_new_info(dirent, ".");
-			ft_lstsort(&list, info, ft_cmp_lexico, 0);
-		}
-	}
-	closedir (dir);
-	while (list)
-	{
-		ft_putstr(((t_info *)(list->content))->mode);
-		ft_putchar('\t');
-		ft_putstr(((t_info *)(list->content))->link);
-		ft_putchar('\t');
-		ft_putstr(((t_info *)(list->content))->usr);
-		ft_putchar('\t');
-		ft_putstr(((t_info *)(list->content))->grp);
-		ft_putchar('\t');
-		ft_putstr(((t_info *)(list->content))->size);
-		ft_putchar('\t');
-		ft_putstr(((t_info *)(list->content))->time);
-		ft_putchar('\t');
-		ft_putstr(((t_info *)(list->content))->name);
-		ft_putchar('\n');
-		list = list->next;
+	n_dir = parser(argv);
+	nb_dir = ft_lstsize(&index);
+	while (n_dir->next)
+	{	
+		ft_ls(n_dir->content, opt, nb_dir);
+		n_dir = n_dir->next;
 	}
 	return (0);
 }
